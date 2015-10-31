bc18214dadca6cb34949be2ac7790800e3cf4573
Valter Kasper <kaspervalter@gmail.com>
2015-10-31 11:42:09 +0100

    Styled bookmark list

--- app/src/main/java/sk/kasper/movieapp/ui/bookmarks/BookmarksListActivity.java
+++ app/src/main/java/sk/kasper/movieapp/ui/bookmarks/BookmarksListActivity.java
@@ -28,18 +28,23 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnItemLongClick;
 import sk.kasper.movieapp.R;
 import sk.kasper.movieapp.Utils;
 import sk.kasper.movieapp.models.Movie;
@@ -65,16 +70,33 @@ public class BookmarksListActivity
 
         bookmarksStorage = new BookmarksStorage(Utils.getSharedPrefs(this));
         lvBookmarks.setAdapter(new BookmarksAdapter(bookmarksStorage.loadBookmarks(), this));
-    }
+		registerForContextMenu(lvBookmarks);
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+									ContextMenu.ContextMenuInfo menuInfo) {
+		if (v.getId() == R.id.lvBookmarks) {
+			AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+			menu.setHeaderTitle(bookmarksStorage.loadBookmarks().get(info.position).name);
+			menu.add("Remove from bookmarks");
+		}
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+		bookmarkRemove(info.position);
+		return true;
+	}
 
     @Override
     public int getDrawerId() {
         return BOOKMARKS_ACTIVITY;
     }
 
-    @OnItemLongClick(R.id.lvBookmarks)
-    public boolean bookmarkLongClick(int position) {
-        Movie movie = (Movie) lvBookmarks.getAdapter().getItem(position);
+	public boolean bookmarkRemove(int position) {
+		Movie movie = (Movie) lvBookmarks.getAdapter().getItem(position);
 		movie.bookmarked = false;
         bookmarksStorage.bookmarkMovie(movie);
         lvBookmarks.setAdapter(new BookmarksAdapter(bookmarksStorage.loadBookmarks(), this));
@@ -86,10 +108,12 @@ public class BookmarksListActivity
             extends BaseAdapter {
 
 		private final List<Movie> bookmarks;
+		private final Context context;
 		private final LayoutInflater inflater;
 
 		private BookmarksAdapter(final List<Movie> bookmarks, final Context context) {
 			this.bookmarks = bookmarks;
+			this.context = context;
 			this.inflater = ((Activity) context).getLayoutInflater();
 		}
 
@@ -123,6 +147,10 @@ public class BookmarksListActivity
 			Movie movie = (Movie) getItem(i);
 			holder.tvTitle.setText(movie.name);
 			holder.tvType.setText(movie.genre);
+			Picasso
+					.with(context)
+					.load(movie.coverUrl)
+					.into(holder.ivCover);
 
 			return convertView;
 		}
@@ -133,6 +161,8 @@ public class BookmarksListActivity
 			public TextView tvTitle;
 			@Bind(R.id.tvType)
 			public TextView tvType;
+			@Bind(R.id.ivCover)
+			public ImageView ivCover;
 
 			public ViewHolder(final View convertView) {
 				ButterKnife.bind(this, convertView);
